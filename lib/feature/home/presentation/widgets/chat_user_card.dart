8d28807e6328ed5fdf63205343ae5605a6e341f2
Daniel Augusto <danielrgb2@gmail.com>
2024-01-09 21:00:37 -0300

    feat: show image profile

--- lib/feature/home/presentation/widgets/chat_user_card.dart
+++ lib/feature/home/presentation/widgets/chat_user_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chat_app/feature/home/data/model/chat_user_model.dart';
 import 'package:chat_app/main.dart';
 import 'package:flutter/cupertino.dart';
@@ -22,9 +23,19 @@ class _ChatUserCardState extends State<ChatUserCard> {
       elevation: .5,
       child: InkWell(
         onTap: () {},
-        child:  ListTile(
+        child: ListTile(
           //user profile picture
-          leading: CircleAvatar(child: Icon(CupertinoIcons.person)),
+          // leading: ,
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(mq.height * .3),
+            child: CachedNetworkImage(
+              width: mq.height * .055,
+              height: mq.height * .055,
+              imageUrl: widget.user.image,
+              errorWidget: (context, url, error) =>
+                  const CircleAvatar(child: Icon(CupertinoIcons.person)),
+            ),
+          ),
 
           //user name
           title: Text(widget.user.name),
@@ -33,7 +44,14 @@ class _ChatUserCardState extends State<ChatUserCard> {
           subtitle: Text(widget.user.about, maxLines: 1),
 
           //last message time
-          trailing: Text('12:00 PM', style: TextStyle(color: Colors.black)),
+          // trailing: Text('12:00 PM', style: TextStyle(color: Colors.black)),
+          trailing: Container(
+            width: 15,
+            height: 15,
+            decoration: BoxDecoration(
+                color: Colors.lightGreenAccent.shade400,
+                borderRadius: BorderRadius.circular(10)),
+          ),
         ),
       ),
     );
