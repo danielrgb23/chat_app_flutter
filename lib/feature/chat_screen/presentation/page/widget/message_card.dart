f57e8592cfc66a648035db9a1cf790ae1cba5d59
Daniel Augusto <danielrgb2@gmail.com>
2024-02-20 09:51:06 -0300

    feat: design message receive widget

--- lib/feature/chat_screen/presentation/page/widget/message_card.dart
+++ lib/feature/chat_screen/presentation/page/widget/message_card.dart
@@ -1,8 +1,14 @@
+import 'package:chat_app/core/apis/apis.dart';
+import 'package:chat_app/main.dart';
+import 'package:chat_app/models/message_model.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 // for showing single message details
 class MessageCard extends StatefulWidget {
-  const MessageCard({Key? key}) : super(key: key);
+  const MessageCard({Key? key, required this.message}) : super(key: key);
+
+  final MessageModel message;
 
   @override
   State<MessageCard> createState() => _MessageCardState();
@@ -11,6 +17,97 @@ class MessageCard extends StatefulWidget {
 class _MessageCardState extends State<MessageCard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return APIs.user.uid == widget.message.fromId
+        ? _greenMessage()
+        : _blueMessage();
+  }
+
+  //send or another user message
+  Widget _blueMessage() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        //message content
+        Flexible(
+          child: Container(
+            padding: EdgeInsets.all(mq.width * .04),
+            margin: EdgeInsets.symmetric(
+                horizontal: mq.width * .04, vertical: mq.height * .01),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 221, 245, 255),
+                border: Border.all(color: Colors.lightBlue),
+                // making borders curved
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(30))),
+            child: Text(
+              widget.message.msg,
+              style: const TextStyle(fontSize: 15, color: Colors.black87),
+            ),
+          ),
+        ),
+
+        //message time
+        Padding(
+          padding: EdgeInsets.only(right: mq.width * .04),
+          child: Text(
+            widget.message.sent,
+            style: const TextStyle(color: Colors.black54, fontSize: 13),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // our or user message
+  Widget _greenMessage() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        //message time
+        Row(
+          children: [
+            //from adding some space
+            SizedBox(
+              width: mq.width * .04,
+            ),
+
+            //double tick blue icon for message read
+            const Icon(Icons.done_all_rounded, color: Colors.blue, size: 20),
+
+            //for adding some space
+            const SizedBox(width: 2),
+
+            //read time
+            Text(
+              "${widget.message.read}${widget.message.sent}",
+              style: const TextStyle(color: Colors.black54, fontSize: 13),
+            ),
+          ],
+        ),
+
+        //message content
+        Flexible(
+          child: Container(
+            padding: EdgeInsets.all(mq.width * .04),
+            margin: EdgeInsets.symmetric(
+                horizontal: mq.width * .04, vertical: mq.height * .01),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 218, 255, 176),
+                border: Border.all(color: Colors.lightGreen),
+                // making borders curved
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30))),
+            child: Text(
+              widget.message.msg,
+              style: const TextStyle(fontSize: 15, color: Colors.black87),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
